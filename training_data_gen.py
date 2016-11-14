d4f1a54b966818c388d2b0e36c2f53c454cd4454
Kai Xu <xukai921110@gmail.com>
2016-11-14 20:12:19 +0000

    Training data done

--- training_data_gen.py
+++ training_data_gen.py
@@ -1,10 +1,31 @@
-# Read number of data points from STDIN
+from subprocess import Popen, PIPE
 
-for i in range(n):
-    # Gen a random graph
+N = 1000
+NODE_NUM = 5
+EDGE_NUM = 15
+WEIGHT_MAX = 100
 
-    # Run Disjkstra's
+f = open('training.txt', 'w')
 
-    # Find the maximum distance
+for i in range(N):
+    # Run random graph generator
+    p1 = Popen(["python", "rand_graph_gen.py", str(NODE_NUM), str(EDGE_NUM), str(WEIGHT_MAX)], stdout=PIPE)
+    rand_graph, _ = p1.communicate()
+
+    # Run Dijkstra's
+    p2 = Popen(["python", "dijkstra.py"], stdin=PIPE, stdout=PIPE)
+    dij_output, _ = p2.communicate(input=rand_graph)
+
+    # Turn output to a vector; fxxk Neil
+    data_vec = map(lambda s: int(s), ", ".join(dij_output.split("\n")).split(", ")[:-1])
+
+    # Find the maximum distance from the source
+    dis_max = max(data_vec[1:])
+
+    # Only edges
+    edges = " ".join(map( lambda i: str(i), data_vec[1:]))
 
     # Output
+    f.write("{edges} {dis_max}\n".format(edges=edges, dis_max=dis_max))
+
+f.close()
